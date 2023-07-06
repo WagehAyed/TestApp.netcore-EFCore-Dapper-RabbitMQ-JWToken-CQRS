8c14a77d2004f21e1df64a7862359621fbeed7b3
Wageh Ayed <wageh.ayed@gmail.com>
2023-07-06 09:41:26 +0300

    Create Dapper DB Projects & Data Seeds

---
+++ DapperDatabase/Dataseeds/Login.Seed.sql
@@ -0,0 +1,10 @@
+﻿Merge Login As Target
+using (
+select * from (
+SELECT TOP 20 'user' + CAST(ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) as nvarchar) + '@example.com' as Email, 'password' + CAST(ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) as nvarchar)  as Password
+FROM sys.all_columns
+) as t
+) as source
+on (source.Email=target.Email)
+when not matched by target
+Then insert (Email,Password) values(source.Email,source.password);
---
+++ DapperDatabase/StoredProcedures/LoginProcedure.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE LoginProcedure
+@flag nvarchar(50),
+@id int=null
+AS
+BEGIN
+	
+	if @flag ='read'
+	Begin
+	select * from Login;
+	end
+	else if @flag ='delete'
+	Begin
+	Delete From Login Where Id=@id;
+	end
+END
+GO
